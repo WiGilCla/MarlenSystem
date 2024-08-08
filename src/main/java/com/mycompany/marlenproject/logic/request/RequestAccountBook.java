fa93c9e80dc1908f2126a9db5cbfb5ed10de3935
WILLIAM <gilclavijowilliam@gmail.com>
2024-08-08 15:20:40 -0500

    Feat: add controllers to save a book and records

---
+++ src/main/java/com/mycompany/marlenproject/logic/request/RequestAccountBook.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.marlenproject.logic.request;
+
+import com.mycompany.marlenproject.logic.AccountBook;
+import com.mycompany.marlenproject.logic.AccountBookRecords;
+import com.mycompany.marlenproject.logic.CheckFields;
+import com.mycompany.marlenproject.persistence.controller.AccountBookController;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
+public class RequestAccountBook {
+    private final AccountBookController accountBookController = new AccountBookController();
+    private final CheckFields CHECKER = new CheckFields();
+    
+    public void saveBook(int accountBookId,Date creationDate, String titleBook, 
+            List<AccountBookRecords> listBookRecords) throws Exception{
+        
+        int bookId = accountBookId;
+        Timestamp creationBook = new Timestamp(creationDate.getTime());
+        String bookTitle = CHECKER.capitalizedString(titleBook);
+        List<AccountBookRecords> bookRecords = listBookRecords;
+        
+        AccountBook book = new AccountBook(bookId, creationDate, titleBook, listBookRecords);
+        accountBookController.saveBookController(book);
+    }
+}
---
+++ src/main/java/com/mycompany/marlenproject/logic/request/RequestAccountBookRecord.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.marlenproject.logic.request;
+
+import com.mycompany.marlenproject.logic.AccountBook;
+import com.mycompany.marlenproject.logic.AccountBookRecords;
+import com.mycompany.marlenproject.logic.CheckFields;
+import com.mycompany.marlenproject.persistence.controller.AccountBookRecordsController;
+
+public class RequestAccountBookRecord {
+    private final AccountBookRecordsController accountBookRecordsController = new AccountBookRecordsController();
+    private final CheckFields CHECKER = new CheckFields();
+    
+    public void saveBookRecord(AccountBook accountBookId,String description,
+            long cashInflow,long cashExpenses) throws Exception{
+        
+        AccountBook book = accountBookId;
+        String recordDescription = CHECKER.capitalizedString(description);
+        long income = cashInflow;
+        long expense = cashExpenses;
+        
+        AccountBookRecords bookRecord = new AccountBookRecords(book, 
+                recordDescription, income, expense);
+        
+        accountBookRecordsController.saveBookRecordController(bookRecord);
+    }
+}
